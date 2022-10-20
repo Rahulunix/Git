2fa5d7ed2aa4c3fb33d90c1d002c3ff78c31afb3
Rahul <rahulktr.singh@gmail.com>
2022-10-20 10:48:17 +0530

    commit

--- code.py
+++ code.py
@@ -1,4 +1,20 @@
-print("Hello world")
-print("twinkle")
-print("twinkle\n")
-print("hey")
+player_guess = int(input("Enter the number"))
+
+no = 17
+no_of_guesses = n
+
+if player_guess == no:
+    print("you guessed correctly")
+elif player_guess < 1 or player_guess > 100:
+    print("OUT OF BOUND")
+elif abs(no - player_guess) < 10:
+    print("WARM")
+elif abs(no - player_guess) > 10:
+    print("COLD")
+
+elif abs(no - player_guess) < 9:
+    print("WARMER")
+elif abs(no - player_guess) > 11 :
+    print("WARMER")
+    
+
---
+++ practice.py
@@ -0,0 +1,8 @@
+l1 = ["rahul","rishi"]
+
+name = input("Enter the name")
+
+if name in l1:
+    print("yes present")
+else:
+    print("NO")
