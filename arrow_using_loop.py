b0f49d3d3b103955544332cdec60d826ccbf3e0a
Rahul kumar <109240162+Rahulunix@users.noreply.github.com>
2023-09-07 18:56:10 +0530

    Create arrow_using_loop.py

---
+++ arrow_using_loop.py
@@ -0,0 +1,10 @@
+n = int(input())
+for i in range(n):
+    for j in range(i):
+        print("*",end = "")
+    print()
+
+for i in reversed(range(n)):
+    for j in reversed(range(i)):
+        print("*",end = "")
+    print()
